1f2a5344d7d2234742888e305be7d945a88939ae
Kostič <sharkbay@email.cz>
2021-03-11 20:09:16 +0100

    react snap v5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { hydrate, render } from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
 
 const rootElement = document.getElementById("root");
+
 if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
+  ReactDOM.hydrate(<App />, rootElement);
 } else {
-  render(<App />, rootElement);
+  ReactDOM.render(<App />, rootElement);
 }
-registerServiceWorker();
